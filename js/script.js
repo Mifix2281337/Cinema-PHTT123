7bba03540d743de9d0136d07f0d649257279f354
Mifix2281337 <136642172+Mifix2281337@users.noreply.github.com>
2025-09-25 21:30:11 +0500

    Добавил интерактив

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const schemeSvg = document.querySelector(".scheme-svg")
+const totalPriceTag = document.querySelector(".price-total");
+let cost = 160;
+let totalPrice = 0;
+schemeSvg.addEventListener('click', (event) => {
+    if (!event.target.classList.contains('booked'))
+    {
+        event.target.classList.toggle("activ")
+        let totalSeats = schemeSvg.querySelectorAll('.activ').length;
+        totalPrice = totalSeats * cost;
+        totalPriceTag.textContent = totalPrice;
+        console.log()
+    }
+});
