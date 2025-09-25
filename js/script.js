eb749de2cd832d70b5e3eb329bc5e3891e30f047
Mifix2281337 <136642172+Mifix2281337@users.noreply.github.com>
2025-09-25 21:53:29 +0500

    Мелкие изменения

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const schemeSvg = document.querySelector(".scheme-svg")
 const totalPriceTag = document.querySelector(".price-total");
-let cost = 160;
+let cost = 800;
 let totalPrice = 0;
 schemeSvg.addEventListener('click', (event) => {
     if (!event.target.classList.contains('booked'))
